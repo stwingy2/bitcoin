be6e62d6b25509b0e605b23073b6a36604cd383a
John lawley <sywingy@gmail.com>
2020-01-13 09:26:34 +0000

    sort of working

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 	//console.log(req.body.crypto);
 
-	request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', (error, response, body) => {
-		console.log(response.body);
+	const currency = req.body.currency;
+	const crypto = req.body.crypto;
+	request(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${crypto}${currency}`, (error, response, body) => {
+		const data = JSON.parse(body);
+		const price = data.last;
+		// let currency = ' USD';
+		// let crypto = 'Bitcoin';
+		//console.log(price);
+		const date = data.display_timestamp;
+		res.write(`<p>The current date is ${date}</p>`);
+		res.write(`<h1>The current price of ${crypto} is ${price} ${currency}</h1>`);
+		res.send();
 	});
 });
 app.listen(3000, () => console.log('listening'));
